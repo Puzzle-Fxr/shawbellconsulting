a4e12664b2a1e227c3679d89b783e5a6b8557c49
Puzzle-Fxr <caldwellavogo@gmail.com>
2026-07-27 14:02:48 +0000

    filled partner details

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -445,7 +445,7 @@ export const partners = [
    detail: "",
   },
   {
-   name: 'Presidency oF Ghana',
+   name: 'Presidency of Ghana',
    logo: "images/logo/ghanaPresidency.png",
    detail: "",
   },
